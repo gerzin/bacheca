bb681b62898a80c911488b32d08be73f995f5f43
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2026-02-14 18:05:37 +0100

    Add tests

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,24 +1,6 @@
 """Pytest configuration for the backend test suite."""
 
-import django
 import pytest
-from django.conf import settings
-
-
-@pytest.fixture(scope="session", autouse=True)
-def django_db_setup():
-    """Configure Django test database."""
-    settings.DATABASES["default"] = {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": settings.DATABASES["default"]["NAME"],
-        "USER": settings.DATABASES["default"]["USER"],
-        "PASSWORD": settings.DATABASES["default"]["PASSWORD"],
-        "HOST": settings.DATABASES["default"]["HOST"],
-        "PORT": settings.DATABASES["default"]["PORT"],
-        "TEST": {
-            "NAME": "test_bacheca",
-        },
-    }
 
 
 @pytest.fixture
